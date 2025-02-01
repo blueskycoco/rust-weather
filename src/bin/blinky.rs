0d06a8e2bdc290c0b2f94734f8df586cf026170c
rdp <rdp@m51.com>
2025-02-01 16:22:48 +0800

    remove unused code
    Signed-off-by: rdp <rdp@m51.com>

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -56,42 +56,7 @@ async fn usr_cmd(rx: &mut BufferedUartRx<'_>,
         }
     }
 }
-/*
-async fn usr_init(usart: &mut Uart<'_, embassy_stm32::mode::Async>) -> bool {
-    let mut s = [0u8; 128];
-    unwrap!(usart.write("+++".as_bytes()).await);
-    unwrap!(usart.read_until_idle(&mut s).await);
-    unwrap!(usart.write("a".as_bytes()).await);
-    unwrap!(usart.read_until_idle(&mut s).await);
 
-    Timer::after_millis(300).await;
-    usr_cmd(usart, "at+wskey=wpa2psk,aes,DUBB-JcJf-kU4g-C3IY\r", &mut s).await;
-    usr_cmd(usart, "at+wsssid=8848\r", &mut s).await;
-    usr_cmd(usart, "at+wmode=sta\r", &mut s).await;
-    loop {
-        unwrap!(usart.write("at+ping=172.20.10.6\r".as_bytes()).await);
-        loop {
-            unwrap!(usart.read_until_idle(&mut s).await);
-            let str_resp = core::str::from_utf8(&s).unwrap();
-            info!("{}", str_resp);
-            if str_resp.contains("Success") {
-                usr_cmd(usart, "at+wann\r", &mut s).await;
-                usr_cmd(usart, "at+netp=tcp,server,1234,172.20.10.8\r", &mut s)
-                        .await;
-                usr_cmd(usart, "at+netp\r", &mut s).await;
-                //usr_cmd(usart, "at+tcpdis=on\r").await;
-                usr_cmd(usart, "at+tcpdis\r", &mut s).await;
-                Timer::after_millis(100).await;
-                return true;
-            } else if str_resp.contains("+ok") || str_resp.contains("+ERR") {
-                break;
-            }
-            clear(&mut s);
-        }
-        Timer::after_millis(1000).await;
-    }
-}
-*/
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let mut config = embassy_stm32::Config::default();
@@ -125,8 +90,6 @@ async fn main(spawner: Spawner) {
     let usart = BufferedUart::new(p.USART2, Irqs, p.PA3, p.PA2, tx_buf,
                                       rx_buf, config).unwrap();
     let (mut usr_tx, mut usr_rx) = usart.split();
-    //let mut usart = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, p.DMA1_CH7,
-    //p.DMA1_CH6, config).unwrap();
     let mut rst = Output::new(p.PA0, Level::High, Speed::Low);
 
     // reset usr_wifi232_t
@@ -146,8 +109,6 @@ async fn main(spawner: Spawner) {
     Timer::after_millis(200).await;
     usr_cmd(&mut usr_rx, &mut usr_tx, "at+wmode=sta\r", &mut s).await;
     usr_cmd(&mut usr_rx, &mut usr_tx, "at+wmode=sta\r", &mut s).await;
-    //usr_cmd(&mut usr_rx, &mut usr_tx, "at+wsssid=CMCC-333\r", &mut s).await;
-    //usr_cmd(&mut usr_rx, &mut usr_tx, "at+wskey=wpa2psk,aes,fei520fei1314\r", &mut s).await;
     usr_cmd(&mut usr_rx, &mut usr_tx, "at+wsssid\r", &mut s).await;
     usr_cmd(&mut usr_rx, &mut usr_tx, "at+wskey\r", &mut s).await;
     usr_cmd(&mut usr_rx, &mut usr_tx, "at+netp=TCP,Server,1234,192.168.1.10\r", &mut s).await;
@@ -168,21 +129,4 @@ async fn main(spawner: Spawner) {
         }
         Timer::after_millis(2000).await;
     }
-    /*
-    let mut pic  = [0u8; 7840];
-    loop {
-        unwrap!(usr_tx.write_all("send ok".as_bytes()).await);
-        unwrap!(usr_rx.read_exact(&mut pic).await);
-        let mut ctx = Context::new();
-        ctx.read(&pic[22..]);
-        let digest = ctx.finish();
-        let remote_dig = &pic[2..18];
-        if digest != remote_dig {
-            error!("md5 missmatch");
-            error!("L {:?}", digest);
-            error!("R {:?}", remote_dig);
-        } else {
-            info!("pic send ok");
-        }
-    }*/
 }
